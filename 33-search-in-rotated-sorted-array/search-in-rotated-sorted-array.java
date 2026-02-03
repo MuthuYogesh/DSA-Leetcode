7a0c3b4615234db5a5341ee6ea4bc8791220ea8e
Muthu yogesh <95938035+MuthuYogesh@users.noreply.github.com>
2026-02-03 20:33:29 +0530

    Time: 5 ms (1.53%) | Memory: 44.2 MB (7.97%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,24 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1, idx = -1;
-        
+        int start = 0, end = nums.length - 1;
+
         while(start <= end){
             int mid = start + (end - start) / 2;
+            System.out.println("a:" + 1/2);
+            System.out.println(mid);
 
-            if(nums[mid] == target) {idx = mid; break;}
-            if(nums[start] == target) {idx = start; break;}
-            if(nums[end] == target) {idx = end; break;}
-
-            if(nums[start] < nums[mid]){
-                if(target > nums[start] && target < nums[mid]) end = mid - 1;
-                else start = mid + 1;
+            if(nums[mid] == target) return mid;
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && nums[mid] > target){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
             }
             else{
-                if(target > nums[mid] && target < nums[end]) start = mid + 1;
-                else end = mid - 1;
+                if(nums[end] >= target && nums[mid] < target){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
             }
         }
 
-        return idx;
+        return -1;
+
     }
 }
